4299072b0629fae07fd1da7f028e2d3e5bbd065f
Blake Shae <76088958+bshaekos@users.noreply.github.com>
2022-06-23 00:15:03 -0700

    Update DanceMove.cs

--- project_4/DanceMove.cs
+++ project_4/DanceMove.cs
@@ -1,1 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class DanceMove : MonoBehaviour
+{
+    public ParticleSystem confetti;
+
+  private void OnTriggerEnter (Collider other)
+  {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            var em = confetti.emission;
+
+            em.enabled = true;
+            confetti.Play();
+        }
+
+        Debug.Log("Object Entered the trigger");
+  }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        var em = confetti.emission;
+
+        em.enabled = false;
+        confetti.Stop();
+
+        Debug.Log("Object Exited the trigger");
+    }
+}
